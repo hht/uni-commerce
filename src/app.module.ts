decf3facecfe7aa6f7eca617634994c53503614e
hht <hht@163.com>
2022-12-20 11:47:51 +0800

    feat: 添加静态文件及端口配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,18 @@ import { UniCommerceController } from '~/controllers/uni-commerce.controller';
 import { UniCommerceService } from '~/services/uni-commerce.service';
 import { PrismaService } from '~/services/prisma.service';
 import { CronService } from '~/services/cron.service';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 @Module({
-  imports: [ScheduleModule.forRoot()],
+  imports: [
+    ScheduleModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '/public'),
+      exclude: ['/api*'],
+      serveRoot: '/',
+    }),
+  ],
   controllers: [AppController, UniCommerceController],
   providers: [AppService, CronService, PrismaService, UniCommerceService],
 })
